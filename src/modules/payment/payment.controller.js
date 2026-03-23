d15af7f97d89ec34e2a8c205f40da0a0af89bd48
iSMAIL <esmailebrahim7400@gmail.com>
2026-03-23 21:06:17 +0200

    commit : calling service and create session

--- src/modules/payment/payment.controller.js
+++ src/modules/payment/payment.controller.js
@@ -3,6 +3,15 @@ import { asyncHandler } from '../../utils/asyncHandler.js';
 import { sendSuccessResponse } from '../../utils/appResponse.js';
 
 export const createCheckoutSession = asyncHandler(async (req, res, next) => {
+    const { amount, plan } = req.body;
+
+    // Call service to create session
+    const session = await createCheckoutSessionService(req.user, amount, plan);
+
+    return sendSuccessResponse(res, {
+        url: session.url,
+        sessionId: session.id
+    }, 200);
 
 });
 
