c1baa10413f3bc6de2424025ac1298d6c7508de2
MustfaAshraf <mostafa012f@gmail.com>
2026-03-29 16:19:56 +0200

    chore(routes): update admin route handler to use new reports module

--- src/utils/routerHandler.js
+++ src/utils/routerHandler.js
@@ -4,12 +4,10 @@ import notificationRouter from "../modules/notification/notification.routes.js"
 import reportRouter from "../modules/report/report.routes.js"
 import userRouter from "../modules/user/user.routes.js"
 import paymentRouter from "../modules/payment/payment.routes.js"
-import adminRouter from "../modules/admin/admin.routes.js"
-
-// import userRouter from "../modules/user/user.routes.js"
+import adminRouter from "../modules/admin/reports/report.routes.js"
 import chatRouter from "../modules/chat/chat.routes.js";
-// import adminTransRouter from "../modules/admin/transactions/transactions.routes.js";
 import adminTransRouter from "../modules/admin/transactions/transactions.routes.js";
+
 const routerHandler = async (app, express) => {
   app.use("/api/v1/auth", authRouter);
   app.use("/api/v1/match", matchRouter);
@@ -17,23 +15,13 @@ const routerHandler = async (app, express) => {
   app.use("/api/v1/reports", reportRouter);
   app.use("/api/v1/users", userRouter);
   app.use("/api/v1/payments", paymentRouter);
-  // app.use("/api/v1/users", userRouter)
   app.use("/api/v1/chat", chatRouter);
   app.use("/api/v1/admin/transactions", adminTransRouter);
+  app.use("/api/v1/admin/reports", adminRouter)
 
-    app.use("/api/v1/auth", authRouter)
-    app.use("/api/v1/match", matchRouter)
-    app.use("/api/v1/notifications", notificationRouter)
-    app.use("/api/v1/reports", reportRouter)
-    app.use("/api/v1/users", userRouter)
-    app.use("/api/v1/payments", paymentRouter)
-    // app.use("/api/v1/users", userRouter)
-    app.use("/api/v1/chat", chatRouter)
-    app.use("/api/v1/admin", adminRouter)
-
-    app.use("/{*any}", (req, res) => {
-        res.status(404).json({ message: "this Router is not found" });
-    });
+  app.use("/{*any}", (req, res) => {
+    res.status(404).json({ message: "this Router is not found" });
+  });
 };
 
 export default routerHandler;
