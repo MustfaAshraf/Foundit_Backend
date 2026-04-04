7b7df85c87ef5b0cd09c31e4e13ff73550985918
MustfaAshraf <mostafa012f@gmail.com>
2026-04-04 03:27:22 +0200

    Update: DB connection file

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -6,7 +6,6 @@ dns.setServers(["8.8.8.8", "1.1.1.1"]);
  
 export const dbConnection = async () => {
     try {
-        // await mongoose.connect(config.DB_MONGO_ATLAS); // Uses the smart selection
         await mongoose.connect(config.MONGODB_URI); // Uses the smart selection
         console.log(`✅ Database Connected`);
     } catch (error) {
